8e82e78e384d456bcf83bf291537bcdd3a01413d
JuliaFleckenstein <julia.fleckenstein@googlemail.com>
2023-08-09 17:47:20 +0200

    created network

--- src/climate_active_envelopes/__init__.py
+++ src/climate_active_envelopes/__init__.py
@@ -23,5 +23,7 @@ Describe typical classes found in project
 from .sample_module import SampleClassName
 from .assembly import wall_assembly, brick_assembly, CAEElement
 from .export import Exporter
+from .utilities import utilities
 
-__all__ = ['SampleClassName', 'wall_assembly', 'brick_assembly', 'CAEElement', 'Exporter']
+__all__ = ['SampleClassName', 'wall_assembly', 'brick_assembly', 'CAEElement', 'utilities', 'Exporter']
+__all__ = []
