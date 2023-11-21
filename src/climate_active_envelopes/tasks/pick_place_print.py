3901537df7e62f08f404d121cf8c6705f2746900
JuliaFleckenstein <julia.fleckenstein@googlemail.com>
2023-11-21 14:21:28 +0100

    tasks

--- src/climate_active_envelopes/tasks/pick_place_print.py
+++ src/climate_active_envelopes/tasks/pick_place_print.py
@@ -12,14 +12,15 @@ __all__ = [
     "StartConfigurationTask"
     "SetToolTask"
     "PickBrickTask"
+    "SafePositionTask"
+    "PlaceBrickTask"
 ]
 
 class StartConfigurationTask(Task):
     def __init__(self, robot, configuration, key=None):
         super(StartConfigurationTask, self).__init__(key)
         self.robot = robot
-        self.configuration = configuration
-        
+        self.configuration = configuration  
     def run(self, thread_stop):
         ffc.commands.move_to_joints(self.robot, self.configuration)
         print("robot moved to start configuration")
@@ -31,7 +32,6 @@ class SetToolTask(Task):
         super(SetToolTask, self).__init__(key)
         self.robot = robot
         self.tool_name = tool_name
-
     def run(self, stop_thread):
         if self.robot.attached_tool:
             self.tool_name = self.robot.attached_tool.name
@@ -41,18 +41,18 @@ class SetToolTask(Task):
             return True
     
 class PickBrickTask(Task):
-    def __init__(self, robot, cart, pick_frame, key=None):
+    def __init__(self, robot, cart, configuration, pick_frame, key=None):
         super(PickBrickTask, self).__init__(key)
         self.robot = robot
         self.cart = cart
+        self.configuration = configuration
         self.pick_frame = pick_frame
 
     def run(self, stop_thread):
-        # Move to safety_frame
-        T = Translation.from_vector(Vector(0,0,0.2)) 
-        safety_frame = self.pick_frame.transformed(T)
-        ffc.commands.move_to_robtarget(self.robot, safety_frame, self.cart)
-        print ('robot moved to safety frame')
+        # Move to safety_position
+        self.configuration = Configuration((4.200, -1.120, 0.543, 0.491, 0.513, -1.093, -1.517), (2, 0, 0, 0, 0, 0, 0), ('axis_joint', 'joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6'))
+        ffc.commands.move_to_joints(self.robot, self.configuration)
+        print ('robot moved to safety position')
 
         # Open gripper
         ffc.commands.open(self.robot)
@@ -66,99 +66,60 @@ class PickBrickTask(Task):
         ffc.commands.close(self.robot)
         print ('gripper closed')
 
-        # Move back to safety_frame
-        ffc.commands.move_to_robtarget(self.robot, safety_frame, self.cart)
-        print ('robot moved back to safety frame')
+        # Move back to safety_position
+        self.configuration = Configuration((4.200, -1.120, 0.543, 0.491, 0.513, -1.093, -1.517), (2, 0, 0, 0, 0, 0, 0), ('axis_joint', 'joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6'))
+        ffc.commands.move_to_joints(self.robot, self.configuration)
+        print ('robot moved to safety position')
+
+        self.is_completed = True
+        return True
+
+class SafePositionTask(Task):
+    def __init__(self, robot, cart, configuration, key=None):
+        super(SafePositionTask, self).__init__(key)
+        self.robot = robot
+        self.cart = cart
+        self.configuration = configuration
+    def run(self, stop_thread):
+        self.configuration = Configuration((4.200, -1.406, -0.041, -0.146, -0.727, 0.249, -0.859), (2, 0, 0, 0, 0, 0, 0), ('axis_joint', 'joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6'))
+        ffc.commands.move_to_joints(self.robot, self.configuration)
+        print ('robot moved to safety position')
+        self.is_completed = True
+        return True
+    
+class PlaceBrickTask(Task):
+    def __init__(self, robot, cart, configuration, place_frames, key=None):
+        super(PlaceBrickTask, self).__init__(key)
+        self.robot = robot
+        self.cart = cart
+        self.configuration = configuration
+        self.place_frames = place_frames
+
+    def run(self, stop_thread):
+        for place_frame in self.place_frames:
+
+            #Move to safety_position
+            T = Translation.from_vector(Vector(0,0,0.2)) 
+            safety_frame = self.place_frame.transformed(T)
+            ffc.commands.move_to_robtarget(self.robot, safety_frame, self.cart)
+            print ('robot moved to safety_frame')
+
+            #Move to place_frame
+            ffc.commands.move_to_robtarget(self.robot, place_frame, self.cart)
+            print ('robot moved to place_frame')
+
+            # Open gripper
+            ffc.commands.open(self.robot)
+            print ('gripper is open')
+
+            #Move back to safety_position
+            T = Translation.from_vector(Vector(0,0,0.2)) 
+            safety_frame = self.place_frame.transformed(T)
+            ffc.commands.move_to_robtarget(self.robot, safety_frame, self.cart)
+            print ('robot moved to safety_frame')
 
         self.is_completed = True
         return True
     
-
 if __name__ == "__main__":
     pass
-
-
-
-
-
-
-
-        # for frame in self.pick_frame[i]:
-
-        #     ffc.commands.move_to_robtarget(self.robot, frame, self.cart)
-        #     print ('moved to pick up frame')
-
-
-
-
-
-# class PlaceBrickTask(Task):
-#     def __init__(self, robot, cart, place_frames, key=None):
-#         super(PlaceBrickTask, self).__init__(key)
-#         pass
-
-# class PrintMortarTask(Task):
-#     def __init__(self, robot, cart, extrude_frames, key=None):
-#         pass
-
-
-"""
-class ExtrudeSegmentTask(Task):
-    def __init__(self, robot, cart, extrude_frames, key=None):
-        self.robot = robot
-        self.cart = cart
-        self.extrude_frames = extrude_frames
-        
-    def run(self, stop_thread):
-        T = Translation.from_vector(Vector(0,0,0.2)) 
-
-        # Moves to approach frame
-        approach_frame = self.extrude_frames[0].transformed(T)
-        move_to_robtarget(self.robot, approach_frame, self.cart)
-        print ('robot moved to approach frame')
-
-        # Turns valve on
-        open(self.robot)
-        print ('valve is open')
-
-        # Turns pump on
-        
-        print ('pump is on')
-
-        # Moves to extrusion frames
-        for frame in self.extrude_frames:
-            move_to_robtarget(self.robot, frame, self.cart)
-            print ('moved to extrusion frame')
-        
-        # Turns valve off
-        close(self.robot)
-        print ('valve is closed')
-
-        # Turns pump off
-        
-        print ('pump is off')
-
-        # Moves to retract frame
-        retract_frame = self.extrude_frames[-1].transformed(T)
-        move_to_robtarget(self.robot, retract_frame, self.cart)
-
-        self.is_completed = True
-        return True
-
-class StampLayerTask(Task):
-    def __init__(self, robot, cart, stamp_frames, key=None):
-        super(StampLayerTask, self).__init__(key)
-        self.robot = robot
-        self.cart = cart
-        self.stamp_frames = stamp_frames
-        
-    def run(self, stop_thread):
-
-        # Moves to stamping frames
-        for frame in self.stamp_frames:
-            move_to_robtarget(self.robot, frame, self.cart)
-            print ('moved to stamp frame')
-
-        self.is_completed = True 
-        return True
-"""
